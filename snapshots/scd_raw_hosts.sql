95d079b0dc90573e028df66b0a29d384705dccbe
Ritesh Regar <riteshregar31@gmail.com>
2025-09-19 15:32:44 +0530

    singular and generic test

--- snapshots/scd_raw_hosts.sql
+++ snapshots/scd_raw_hosts.sql
@@ -1,10 +1,16 @@
 {% snapshot scd_raw_hosts %}
+
 {{
- config(
- target_schema='DEV',
- unique_key='id',
- strategy='timestamp',
- updated_at='updated_at',
- invalidate_hard_deletes=True
- )
+  config(
+    target_schema='DEV',
+    unique_key='id',
+    strategy='timestamp',
+    updated_at='updated_at',
+    invalidate_hard_deletes=True
+  )
 }}
+
+select * 
+from {{ source('airbnb', 'hosts') }}
+
+{% endsnapshot %}
